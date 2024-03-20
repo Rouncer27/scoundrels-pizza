e075f2fee7551ef13b01da02a336b4ed71db0e1f
Trevor Rounce <trevor@switchbackcreative.ca>
2024-03-20 14:38:45 -0600

    added seo

---
+++ src/data/siteData.js
@@ -0,0 +1,9 @@
+import defaultSrc from "../../public/default.jpg";
+
+export const baseData = {
+  title: "Scoundrels Pizza & Shareables",
+  description:
+    "Scoundrels Pizza & Shareables - Neopolitan pizza with a Brooklyn accent.",
+  siteImg: `https://scoundrels-swb.netlify.app/${defaultSrc.src}`,
+  url: "https://scoundrelspizza.com/",
+};
