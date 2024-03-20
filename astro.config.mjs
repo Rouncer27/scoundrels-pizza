99037002b0d9145e94f5e9fefccf497fa0e18946
Trevor Rounce <trevor@switchbackcreative.ca>
2024-03-20 15:11:30 -0600

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://scoundrelspizza.com",
   image: {
-    domains: ["scoundrels.swbdatabases3.com"],
+    domains: ["scoundrels.swbdatabases3.com"]
   },
+  integrations: [sitemap()]
 });
